e023e81e958b4ad5c7c2b4a11a26d5fa16dfa2ad
sami-at <samiatlagh3@gmail.com>
2024-04-03 16:16:33 +0000

    message

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,57 @@
-// Navbar.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import LanguageSelect from '../LanguageSelect/LanguageSelect';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 const Navbar = () => {
   const { t } = useTranslation();
+  const [showMenu, setShowMenu] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 760);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsMobile = window.innerWidth < 760;
+      setIsMobile(newIsMobile);
+      if (!newIsMobile) {
+        setShowMenu(false); // Close the menu when screen size becomes larger than 760px
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <nav>
       <Link to="/" className="logo">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Honey_Logo_Orange.svg/1280px-Honey_Logo_Orange.svg.png" alt="Logo" />
       </Link>
-      <ul className="navigation-links">
-        <li><Link to="/">{t('navigation.home')}</Link></li>
-        <li><Link to="/products">{t('navigation.products')}</Link></li>
-        <li><Link to="/about">{t('navigation.about')}</Link></li>
-        <li><Link to="/contact">{t('navigation.contact')}</Link></li>
+      <div className="menu-icon" onClick={toggleMenu}>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+      <ul className={`navigation-links ${showMenu ? 'show' : ''}`}>
+        {isMobile && (
+          <div className='navdrawer-top'>
+            <FontAwesomeIcon className="exit-icon" icon={faTimes} onClick={toggleMenu} />
+            <li><LanguageSelect /></li>
+          </div>
+        )}
+        <li><Link to="/" onClick={toggleMenu}>{t('navigation.home')}</Link></li>
+        <li><Link to="/products" onClick={toggleMenu}>{t('navigation.products')}</Link></li>
+        <li><Link to="/about" onClick={toggleMenu}>{t('navigation.about')}</Link></li>
+        <li><Link to="/contact" onClick={toggleMenu}>{t('navigation.contact')}</Link></li>
       </ul>
-      <LanguageSelect />
+      {!isMobile && <LanguageSelect id="navselect" />}
     </nav>
   );
 }
